f22e01272b40545e23f24c9756b2aa225a4b547b
aix-dean <68581352+aix-dean@users.noreply.github.com>
2025-09-11 22:06:31 +0800

    create single notification module for different departments

--- hooks/use-sales-notifications.ts
+++ hooks/use-sales-notifications.ts
@@ -1,2 +1,2 @@
 // This file is deprecated. Use the unified notifications system from @/hooks/use-notifications
-export { useLogisticsNotifications } from "@/hooks/use-notifications"
+export { useSalesNotifications } from "@/hooks/use-notifications"
