bf490060fb3805e1da4abf6c3dba193a68283473
Eugene Lozon <68581352+aix-dean@users.noreply.github.com>
2025-10-24 11:56:45 +0800

    unit testing

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -5,7 +5,7 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom',
+    environment: 'happy-dom',
     setupFiles: ['./__tests__/setup.ts'],
     globals: true,
     include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
