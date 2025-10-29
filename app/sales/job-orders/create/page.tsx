546795ffed68e320176c1bd383aebeea22ce6f82
AIX Dean <dean.aisyndicate.ph@gmail.com>
2025-10-29 21:35:10 +0800

    (update) Sales JO attachment backend

--- app/sales/job-orders/create/page.tsx
+++ app/sales/job-orders/create/page.tsx
@@ -765,13 +765,7 @@ export default function CreateJobOrderPage() {
             campaignName: form.campaignName, // Added campaign name
             requestedBy: `${userData?.first_name} ${userData?.last_name}` || "Auto-Generated",
             remarks: form.remarks,
-            attachments: form.attachmentUrl
-              ? [{
-                url: form.attachmentUrl,
-                name: form.attachmentFile?.name || "Attachment",
-                type: form.attachmentFile?.type || "image",
-              }]
-              : [],
+            attachments: form.materialSpecAttachmentUrl ? { name: form.materialSpecAttachmentFile?.name || "Attachment", type: form.materialSpecAttachmentFile?.type || "image", url: form.materialSpecAttachmentUrl } : null,
             materialSpec: form.materialSpec,
             materialSpecAttachmentUrl: form.materialSpecAttachmentUrl,
             clientCompliance: {
@@ -945,13 +939,7 @@ export default function CreateJobOrderPage() {
         campaignName: form.campaignName,
         requestedBy: userData?.first_name || "Auto-Generated",
         remarks: form.remarks,
-        attachments: form.attachmentUrl
-          ? [{
-              url: form.attachmentUrl,
-              name: form.attachmentFile?.name || "Attachment",
-              type: form.attachmentFile?.type || "image",
-            }]
-          : [],
+        attachments: form.materialSpecAttachmentUrl ? { name: form.materialSpecAttachmentFile?.name || "Attachment", type: form.materialSpecAttachmentFile?.type || "image", url: form.materialSpecAttachmentUrl } : null,
         materialSpec: form.materialSpec,
         materialSpecAttachmentUrl: form.materialSpecAttachmentUrl,
         clientCompliance: {
